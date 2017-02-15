a4e6094edf69c48b50566fcb53f9ff2ee3f0758b
Alex Aguirre <alex.y2kare@gmail.com>
2017-02-15 07:05:49 +0000

    Initial routing

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,22 @@
 class PagesController < ApplicationController
   def home
   end
+  
+  def index
+  end
+  
+  def mollusca
+  end
+  
+  def brachiopoda
+  end
+  
+  def echinodermata
+  end
+  
+  def arthropod
+  end
+  
+  def foraminifera
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,17 @@ Rails.application.routes.draw do
 
 #   resources :post
   root to: "pages#home"
+  get 'index', to: 'pages#index'  
   
+  get 'index/mollusca', to: 'pages#mollusca'  
+  get 'index/brachiopoda', to: 'pages#brachiopoda'  
+  get 'index/echinodermata', to: 'pages#echinodermata'  
+  get 'index/arthropod', to: 'pages#arthropod'  
+  get 'index/foraminifera', to: 'pages#foraminifera'  
+  
+
   resources :fossils
+  
   
   
 end
