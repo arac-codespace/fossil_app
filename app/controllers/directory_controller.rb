7176c858a7c97b64f7e0c61d9d1d615aecdff922
Alex Aguirre <alex.y2kare@gmail.com>
2017-02-17 04:44:11 +0000

    Rough setup of the directory index and show view/controllers

--- app/controllers/directory_controller.rb
+++ app/controllers/directory_controller.rb
@@ -4,17 +4,35 @@ class DirectoryController < ApplicationController
   def index
     @phylums = Phylum.all
     @classes = FossilClass.all
+    # In case the id of the Kingdom changes on a future db update, I loop for the
+    # appropiate kingdom by name and invoke the id in the view file.
     @animalia = Kingdom.find_by kingdom_name: 'Animalia'
     @protozoa = Kingdom.find_by kingdom_name: 'Protozoa'
   end
   
   def show
-    @p_id = params[:id]
-    @name_find = Phylum.find_by id: @p_id
-    @name = @name_find
-    @fossils = Fossil.where phylum_id: @p_id
+    # Phylum param assigned from the index file to url route.  Phylum.id
+    @phylum_id = params[:id]
+    # Finds the phylum record that matches the id param forwarded from the index. 
+    @phylum_find = Phylum.find_by id: @phylum_id
+    # These are all the fossils that share the phylum_id with the index parameter.
+    @fossils = Fossil.where phylum_id: @phylum_id
+    # Redirects if the expected Phylum.id param is not found in the db...
+    redirect_to directory_index_path if @phylum_find.nil?
+    
+    # IF there is a class_var query string...
+    if params[:class_var]
+      # Query string instance var...
+      @class_var = params[:class_var]
+      # Find the class record whose name matches the query string
+      @class = FossilClass.find_by class_name: @class_var 
+      # Of the expected FossilClass.class_name query string is not found in the db...
+      redirect_to directory_index_path if @class.nil?
+    end
+    
     
   end
   
   
 end
+  
